6feade2d29bee851f51acd0af4557564a5ca588b
Alexander Mityukov <a.m.mityukov@gmail.com>
2023-03-04 15:50:03 +0100

    Fix variables and functions names

--- vk_comics/vk_comics.py
+++ vk_comics/vk_comics.py
@@ -5,18 +5,18 @@ import requests
 from urllib import parse
 
 
-def fetch_xkcd_comics() -> tuple[BytesIO, str]:
-    comics_id: int = randint(1, 500)
-    comics_info_url: str = f"https://xkcd.com/{comics_id}/info.0.json"
-    with requests.get(comics_info_url) as response:
+def fetch_xkcd_comic() -> tuple[BytesIO, str]:
+    comic_id: int = randint(1, 500)
+    comic_info_url: str = f"https://xkcd.com/{comic_id}/info.0.json"
+    with requests.get(comic_info_url) as response:
         response.raise_for_status()
-        comics_info: dict = response.json()
-        comics_image_url: str = comics_info['img']
-        comics_comment: str = comics_info['alt']
-    with requests.get(comics_image_url) as response:
+        response: dict = response.json()
+        comic_image_url: str = response['img']
+        comic_comment: str = response['alt']
+    with requests.get(comic_image_url) as response:
         response.raise_for_status()
-        comics_image: BytesIO = BytesIO(response.content)
-    return comics_image, comics_comment
+        comic_image: BytesIO = BytesIO(response.content)
+    return comic_image, comic_comment
 
 
 class VkAPIerror(Exception):
@@ -42,29 +42,30 @@ def get_wall_upload_server_url() -> str:
     }
     with requests.get(wall_upload_api_url, params) as response:
         response.raise_for_status()
-        response_json: dict = response.json()
-    raise_vk_response_for_error(response_json)
-    return response_json['response']['upload_url']
+        response: dict = response.json()
+    raise_vk_response_for_error(response)
+    return response['response']['upload_url']
 
 
-def upload_comics_image(comics_wall_upload_server_url: str,
-                        comics_image: BytesIO) -> tuple[str, str, str]:
+def upload_comic_image(comic_wall_upload_server_url: str,
+                       comic_image: BytesIO) -> tuple[str, str, str]:
     files: dict[str, tuple[str, bytes]] = {'photo': ('comics.png',
-                                           comics_image.getvalue())}
-    comics_image.close()
-    with requests.post(comics_wall_upload_server_url,
+                                           comic_image.getvalue())}
+    comic_image.close()
+    with requests.post(comic_wall_upload_server_url,
                        files=files) as response:
         response.raise_for_status()
-        response_json: dict = response.json()
-    raise_vk_response_for_error(response_json)
-    return (response_json['server'],
-            response_json['photo'],
-            response_json['hash'])
+        response: dict = response.json()
+    raise_vk_response_for_error(response)
+    vk_server: str = response['server']
+    comic_image_attributes: str = response['photo']
+    comic_image_hash: str = response['hash']
+    return vk_server, comic_image_attributes, comic_image_hash
 
 
-def save_wall_comics_image(vk_server: str,
-                           comics_json: str,
-                           comics_hash: str) -> tuple[str, str]:
+def save_wall_comic_image(vk_server: str,
+                          comic_image_attributes: str,
+                          comic_image_hash: str) -> tuple[str, str]:
     vk_api_url: str = 'https://api.vk.com/method/'
     method_name: str = 'photos.saveWallPhoto'
     save_wall_photo_api_url: str = parse.urljoin(vk_api_url, method_name)
@@ -73,20 +74,21 @@ def save_wall_comics_image(vk_server: str,
         'group_id': main.vk_group_id,
         'v': '5.81',
         'server': vk_server,
-        'photo': comics_json,
-        'hash': comics_hash,
+        'photo': comic_image_attributes,
+        'hash': comic_image_hash,
     }
     with requests.post(save_wall_photo_api_url, params=params) as response:
         response.raise_for_status()
-        response_json: dict = response.json()
-    raise_vk_response_for_error(response_json)
-    return (response_json['response'][0]['id'],
-            response_json['response'][0]['owner_id'])
+        response: dict = response.json()
+    raise_vk_response_for_error(response)
+    comic_image_id: str = response['response'][0]['id']
+    comic_image_owner_id: str = response['response'][0]['owner_id']
+    return comic_image_id, comic_image_owner_id
 
 
-def public_post_on_wall(comics_comment: str,
-                        comics_image_id: str,
-                        comics_image_owner_id: str) -> None:
+def publish_post_on_wall(comics_comment: str,
+                         comics_image_id: str,
+                         comics_image_owner_id: str) -> None:
     vk_api_url: str = 'https://api.vk.com/method/'
     method_name: str = 'wall.post'
     public_pos_on_wall_api_url: str = parse.urljoin(vk_api_url, method_name)
@@ -100,32 +102,32 @@ def public_post_on_wall(comics_comment: str,
     }
     with requests.post(public_pos_on_wall_api_url, params=params) as response:
         response.raise_for_status()
-        response_json: dict = response.json()
-    raise_vk_response_for_error(response_json)
+        response: dict = response.json()
+    raise_vk_response_for_error(response)
 
 
 def main() -> None:
     env: Env = Env()
     env.read_env()
-    main.vk_access_token: str = env('API_TOKEN')
-    main.vk_group_id: str = env('GROUP_ID')
-    comics_image: BytesIO
-    comics_comment: str
-    comics_image, comics_comment = fetch_xkcd_comics()
-    comics_wall_upload_server_url: str = get_wall_upload_server_url()
-    comics_hash: str
-    comics_json: str
+    main.vk_access_token: str = env('VK_API_TOKEN')
+    main.vk_group_id: str = env('VK_GROUP_ID')
+    comic_image: BytesIO
+    comic_comment: str
+    comic_image, comic_comment = fetch_xkcd_comic()
+    comic_wall_upload_server_url: str = get_wall_upload_server_url()
     vk_server: str
-    vk_server, comics_json, comics_hash = upload_comics_image(
-                                                comics_wall_upload_server_url,
-                                                comics_image)
-    comics_image_id: str
-    comics_image_owner_id: str
-    comics_image_id, comics_image_owner_id = save_wall_comics_image(
-                                                                vk_server,
-                                                                comics_json,
-                                                                comics_hash)
-    public_post_on_wall(comics_comment, comics_image_id, comics_image_owner_id)
+    comic_image_attributes: str
+    comic_image_hash: str
+    vk_server, comic_image_attributes, comic_image_hash = upload_comic_image(
+                                                  comic_wall_upload_server_url,
+                                                  comic_image)
+    comic_image_id: str
+    comic_image_owner_id: str
+    comic_image_id, comic_image_owner_id = save_wall_comic_image(
+                                                        vk_server,
+                                                        comic_image_attributes,
+                                                        comic_image_hash)
+    publish_post_on_wall(comic_comment, comic_image_id, comic_image_owner_id)
 
 
 if __name__ == '__main__':
